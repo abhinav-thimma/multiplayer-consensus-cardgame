756bf60e0c85e6f15bedc1dd24f8e811000d0b9a
Abhinav Thimma <athimma2@illinois.edu>
2022-05-15 19:22:07 -0500

    making API call to save Demographic form responses

--- client/src/Demographic/Demographic.jsx
+++ client/src/Demographic/Demographic.jsx
@@ -1,19 +1,21 @@
 import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import { Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 
 import "./Demographic.css";
 
 const Demographic = (props) => {
+  let history = useHistory();
   const { roomId, playerNumber } = props.match.params;
+  const [ validated, setValidated ] = useState(false);
 
   const [dob, setDob] = useState();
   const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("1");
+  const [gender, setGender] = useState("female");
   const [nativespeaker, setNativeSpeaker] = useState("1");
   const [firstLan, setFirstLan] = useState("");
   const [engAcqAge, setEngAcqAge] = useState(0);
-  const [hispOrLatino, setHispOrLatino] = useState("1");
+  const [hispOrLatino, setHispOrLatino] = useState("2");
   const [ethnicity, setEthnicity] = useState("Native American/Alaska Native");
 
   const handleDobChange = (event) => {
@@ -40,17 +42,47 @@ const Demographic = (props) => {
   const handleEthnicityChange = (event) => {
     setEthnicity(event.target.value);
   };
-  const handleJoinRoom = (e) => {
-    e.preventDefault();
-    console.log(dob);
-    console.log(age);
-    console.log(gender);
-    console.log(nativespeaker);
-    console.log(firstLan);
-    console.log(engAcqAge);
-    console.log(hispOrLatino);
-    console.log(ethnicity);
-  }
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      let ethn = ethnicity;
+      console.log(hispOrLatino);
+      if(hispOrLatino === "1") {
+        ethn = "Hispanic or Latino";
+      }
+      let request = {
+        "roomid": roomId,
+        "player_num":  "Player " + playerNumber,
+        "dob": dob,
+        "age": parseInt(age),
+        "gender": gender,
+        "native_eng": parseInt(nativespeaker),
+        "first_lan": firstLan,
+        "eng_acq_age": parseInt(engAcqAge),
+        "ethnicity": ethn
+      };
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request),
+      };
+      fetch('http://127.0.0.1:5000/demographic', options)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+      console.log(request);
+      history.push(`/room/${roomId}/${playerNumber}`);
+    }
+
+    setValidated(true);
+  };
 
   return (
     <div className="demo-container">
@@ -58,28 +90,28 @@ const Demographic = (props) => {
 
       <br/>
       <h4>Please answer the following questions:</h4>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="Form.ControlInput1">
           <Form.Label>Date of birth: </Form.Label>
-          <Form.Control type="date" name='date_of_birth'  onChange={handleDobChange}/>
+          <Form.Control type="date" name='date_of_birth'  onChange={handleDobChange} required/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Form.ControlInput2">
           <Form.Label>Age: </Form.Label>
-          <Form.Control type="number" name='age' onChange={handleAgeChange} />
+          <Form.Control type="number" name='age' onChange={handleAgeChange} required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Form.ControlInput3">
           <Form.Label>Gender: </Form.Label>
-          <Form.Select aria-label="Gender select" value={gender} onChange={handleGenderChange}>
-            <option value="1">Female</option>
-            <option value="2">Male</option>
+          <Form.Select aria-label="Gender select" value={gender} onChange={handleGenderChange} required>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
           </Form.Select>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Form.ControlInput3">
           <Form.Label>Are you a native english speaker? </Form.Label>
-          <Form.Select aria-label="English select" onChange={handleNativeSpeakerChange} value={nativespeaker}>
+          <Form.Select aria-label="English select" onChange={handleNativeSpeakerChange} value={nativespeaker} required>
             <option value="1">Yes</option>
             <option value="2">No</option>
           </Form.Select>
@@ -87,18 +119,18 @@ const Demographic = (props) => {
 
         <Form.Group className="mb-3" controlId="Form.ControlInput2">
           <Form.Label>What is your first language? </Form.Label>
-          <Form.Control type="text" name='first_lan'  onChange={handleFirstLanChange}/>
+          <Form.Control type="text" name='first_lan'  onChange={handleFirstLanChange} required/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Form.ControlInput2">
           <Form.Label>What age did you begin to acquire English? </Form.Label>
-          <Form.Control type="number" name='age_eng' onChange={handleEngAcqAgeChange} />
+          <Form.Control type="number" name='age_eng' onChange={handleEngAcqAgeChange} required/>
         </Form.Group>
 
 
         <Form.Group className="mb-3" controlId="Form.ControlInput3">
           <Form.Label>Are you a Hispanic or Latino? </Form.Label>
-          <Form.Select aria-label="Hispanic select" onChange={handleHispOrLatinoChange} value={hispOrLatino}>
+          <Form.Select aria-label="Hispanic select" onChange={handleHispOrLatinoChange} value={hispOrLatino} required>
             <option value="1">Yes</option>
             <option value="2">No</option>
           </Form.Select>
@@ -116,10 +148,8 @@ const Demographic = (props) => {
           </Form.Select>
         </Form.Group>
         <br/>
+        <Button type="submit">Submit form</Button>
 
-      <Link to={`/room/${roomId}/${playerNumber}`}  className="enter-room-button">
-        Join room
-      </Link>
 
       </Form>
     </div>
