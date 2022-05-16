f6a6cf63f49d3d38c48b16d0faf56eb8c75339d3
Abhinav Thimma <athimma2@illinois.edu>
2022-05-15 19:24:56 -0500

    added a flask app to handle DB related API calls

---
+++ flask-app/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, Response
+from flask_cors import CORS, cross_origin
+from db_util import DBUtil
+import mysql.connector as mysql
+import json
+ 
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+HOST = '127.0.0.1'
+USER = 'root'
+PASSWORD = ''
+DB = 'actionlabstudy_consensus'
+
+db_connection = mysql.connect(host=HOST, database=DB, user=USER, password=PASSWORD)
+dbUtil = DBUtil(db_connection)
+
+@app.route('/')
+@cross_origin()
+def home():
+    return "hello"
+
+@app.route('/demographic', methods=['POST'])
+@cross_origin()
+def store_demographic_info():
+    request_data = request.get_json()
+    result = dbUtil.store_demographic_info(request_data)
+
+    response = {}
+    if(result):
+        response = {"status": "SUCCESS"}
+    else:
+        response = {"status": "FAILURE"}
+    
+    resp = Response(json.dumps(response), status=200, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Content-Type'] = 'application/json'
+
+    return resp
+
+@app.route('/survey', methods=['POST'])
+@cross_origin()
+def store_survey_results():
+    request_data = request.get_json()
+    result = dbUtil.store_survey_response(request_data)
+
+    response = {}
+    if(result):
+        response = {"status": "SUCCESS"}
+    else:
+        response = {"status": "FAILURE"}
+    
+    resp = Response(json.dumps(response), status=200, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Content-Type'] = 'application/json'
+
+    return resp
+    
+
+@app.route('/moves', methods=['POST'])
+@cross_origin()
+def store_player_moves():
+    request_data = request.get_json()
+    result = dbUtil.store_player_moves(request_data)
+
+    response = {}
+    if(result):
+        response = {"status": "SUCCESS"}
+    else:
+        response = {"status": "FAILURE"}
+    
+    resp = Response(json.dumps(response), status=200, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Content-Type'] = 'application/json'
+
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask-app/db_util.py
@@ -0,0 +1,84 @@
+import datetime
+import time
+
+class DBUtil:
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+    
+    def store_demographic_info(self, request_json):
+        try:
+            roomid = request_json['roomid']
+            player_num = request_json['player_num']
+            dob = request_json['dob']
+            age = request_json['age']
+            gender = request_json['gender']
+            native_eng = request_json['native_eng']
+            first_lan = request_json['first_lan']
+            eng_acq_age = request_json['eng_acq_age']
+            ethnicity = request_json['ethnicity']
+
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+            query = f'''
+                INSERT INTO demographic_info (roomid, player_num, dob, age, gender, native_eng, first_lan, eng_acq_age, ethnicity, timestamp)
+                    VALUES ("{roomid}", "{player_num}", "{dob}", {age}, "{gender}", {native_eng}, "{first_lan}", {eng_acq_age}, "{ethnicity}", "{timestamp}") '''
+
+            cursor = self.db_connection.cursor()
+            cursor.execute(query)
+            self.db_connection.commit()
+            cursor.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def store_survey_response(self, request_json):
+        try:
+            roomid = request_json['roomid']
+            game_num = request_json['game_num']
+            round_num = request_json['round_num']
+            player_num = request_json['player_num']
+            q1_res = request_json['q1_res']
+            q2_res = request_json['q2_res']
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+
+            query = f'''
+                INSERT INTO survey_responses (roomid, game_num, round_num, player_num, q1_res, q2_res, timestamp)
+                    VALUES ("{roomid}", {game_num}, {round_num}, "{player_num}", "{q1_res}", "{q2_res}", "{timestamp}") '''
+
+            cursor = self.db_connection.cursor()
+            cursor.execute(query)
+            self.db_connection.commit()
+            cursor.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def store_player_moves(self, request_json):
+        try:
+            roomid = request_json['roomid']
+            game_num = request_json['game_num']
+            round_num = request_json['round_num']
+            player_num = request_json['player_num']
+            card_selected = request_json['card_selected']
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+            query = f'''
+                INSERT INTO player_moves (roomid, game_num, round_num, player_num, card_selected, timestamp)
+                    VALUES ("{roomid}", {game_num}, {round_num}, "{player_num}", "{card_selected}", "{timestamp}") '''
+
+            cursor = self.db_connection.cursor()
+            cursor.execute(query)
+            self.db_connection.commit()
+            cursor.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+
